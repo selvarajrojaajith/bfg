8da32809ba2e034497b878674ee9c59b57cae1b7
selvarajrojaajith <36071813+selvarajrojaajith@users.noreply.github.com>
2018-03-28 16:23:25 +0530

    greater.c

---
+++ greater.c
@@ -0,0 +1,22 @@
+
+#include <iostream>
+using namespace std;
+ 
+int getClosest(int, int, int);
+int Close(int arr[], int n)
+{
+    if (target <= arr[0])
+        return arr[0];
+    if (target >= arr[n - 1])
+        return arr[n - 1];
+{
+    int i = 0, j = n, mid = 0;
+    while (i < j) {
+        mid = (i + j) / 2;
+ }
+ }
+        if (arr[mid] == target)
+            return arr[mid];
+
+}
+ 
